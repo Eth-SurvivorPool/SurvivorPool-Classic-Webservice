b0bf76437224b70c555c030983f52137a5a5678e
Dat Nguyen <dat.nguyen@ge.com>
2018-07-19 10:03:40 -0700

    setting clock to 1 hr

--- game/game-engine.js
+++ game/game-engine.js
@@ -69,7 +69,7 @@ var settleGameJob = scheduler.scheduleJob('* * 0 * * *', async () => {
 	exports.settleGame();
 });
 
-var infectJob = scheduler.scheduleJob('0 */2 * * *', async () => {
+var infectJob = scheduler.scheduleJob('0 * * * *', async () => {
 	exports.infectRandomPlayer();
 });
 
